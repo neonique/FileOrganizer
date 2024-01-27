ea4e30710e5db985734a027e21563e15e0b68bf6
neonique <nickgarczorz@gmail.com>
2024-01-27 22:35:18 +0100

    added test for TestSourceLoaderFromFolder

--- src/test/java/de/neonique/persistence/source_loader/TestSourceLoaderFromFolder.java
+++ src/test/java/de/neonique/persistence/source_loader/TestSourceLoaderFromFolder.java
@@ -1,19 +1,35 @@
 package de.neonique.persistence.source_loader;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TestSourceLoaderFromFolder {
 
-    @BeforeAll
-    static void init(){
-
+    SourceLoader srcLoader;
+    @BeforeEach
+    void init(){
+        srcLoader = new SourceLoaderFromFolder();
     }
     @Test()
-    @DisplayName("Test")
+    @DisplayName("get default src path, when no custom path is configured")
     void test_01(){
+        String expectedPath = Paths.get(System.getProperty("user.home") + "/Desktop/").toString();
+
+        String srcPath = srcLoader.getPath();
+
+        assertThat(expectedPath).isEqualTo(srcPath);
+    }
+/*
+    @Test()
+    @DisplayName("get custom path if it is configured")
+    void test_02(){
+        String expectedPath = Paths.get(System.getProperty("user.home") + "/Desktop/").toString();
 
     }
-
+*/
 }
