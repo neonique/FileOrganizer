7a1afc7da8b8c2db3d3bf1ecdd7dd736c6d0b8eb
neonique <nickgarczorz@gmail.com>
2024-01-27 14:25:30 +0100

    fixed faulty directory structure and dependencies

---
+++ src/test/java/de/neonique/source_loader/TestSourceLoaderFromFolder.java
@@ -0,0 +1,13 @@
+package de.neonique.source_loader;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestSourceLoaderFromFolder {
+    @Test()
+    @DisplayName("Test")
+    void test_01(){
+
+    }
+
+}
